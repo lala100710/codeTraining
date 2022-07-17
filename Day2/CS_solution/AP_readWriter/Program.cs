3063bfe7ae9ca8f47e26e730c9662cc01e499f68
lala0126 <a21219338@gmail.com>
2022-07-17 17:58:44 +0800

    Day2 寫入讀檔

---
+++ Day2/CS_solution/AP_readWriter/Program.cs
@@ -0,0 +1,13 @@
+﻿//寫入
+StreamWriter streamWriter = new StreamWriter("C:/Users/USER/Desktop/帳號.txt");
+for (int i = 0; i < 10; i++)
+    streamWriter.WriteLine("hello {0}",i);
+   streamWriter.Close();
+
+//讀取
+StreamReader streamReader = new StreamReader(@"C:/Users/USER/Desktop/帳號.txt");
+for(int i = 0; i < 10; i++)
+
+    Console.WriteLine(streamReader.ReadToEnd()); 
+
+streamReader.Close();
